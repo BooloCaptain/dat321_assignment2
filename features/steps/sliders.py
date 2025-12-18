ec34283dc815d2f43036620c04343e9a47a6b750
Astrid Emerath <astrid.emerath@gmail.com>
2025-12-18 17:59:51 +0100

    implement scenario
    make setup step more general

--- features/steps/sliders.py
+++ features/steps/sliders.py
@@ -6,13 +6,19 @@ from model.algorithm.nn_model.nn_alg import NNAlg
 from model.demo_model import DemoModel
 from model.timer import Timer
 
+# global variable to keep app instance in so it only needs setting up once
+app_instance = None
+
 @given('the app is running')
 def step_impl(context):
-    start_time = datetime.now().replace(hour=0,minute=0,second=0)
-    timer = Timer()
-    scheduler = NNAlg()
-    model = DemoModel(scheduler, timer, start_time)
-    context.app = Application(model, start_time)
+    global app_instance
+    if app_instance is None:
+        start_time = datetime.now().replace(hour=0,minute=0,second=0)
+        timer = Timer()
+        scheduler = NNAlg()
+        model = DemoModel(scheduler, timer, start_time)
+        app_instance = Application(model, start_time)
+    context.app = app_instance
 
 @when('the slider is dragged more than half way to the right')
 def step_impl(context):
@@ -24,3 +30,13 @@ def step_impl(context):
 def step_impl(context):
     time = context.app.model.get_time()
     assert time.hour >= 12 and time.minute > 0
+
+@when('the slider is dragged left')
+def step_impl(context):
+    context.app.slider._slider.set(0)  # simulate dragging the slider 
+    context.app.slider.set_time(None)    # trigger the time update
+
+@then('the time will be 00:00')
+def step_impl(context):
+    time = context.app.model.get_time()
+    assert time.hour == 0 and time.minute == 0
